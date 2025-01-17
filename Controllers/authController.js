c07c50af0e3b29100667f6bfe37f1a93adf50135
SnehaMgithub <snehamathes2000@gmail.com>
2025-01-17 19:04:38 +0530

    updated

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -102,7 +102,7 @@ export const forgotPassword = async (req, res) => {
       subject: "Password Reset Link",
       text: `You are receiving this because you have requested the reset of the password for your account 
       Please click the following link or paste it into your browser to complete the process
-      http://localhost:5173/reset-password/${user._id}/${token}`,
+      https://hospital-website-frontend.vercel.app/reset-password/${user._id}/${token}`,
     };
     transporter.sendMail(mailOptions, function (error, info) {
       if (error) {
